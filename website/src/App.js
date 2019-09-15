e84091290774f188e18e957ab3c36153d0100448
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-09-15 16:21:15 -0400

    finish functionality

--- website/src/App.js
+++ website/src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+import { Search } from "./components/Search";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Search />
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
